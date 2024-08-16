319b6f037e7482cc74cff7b702746bbcce42024e
zhangshumeng <916406821@qq.com>
2024-08-16 14:51:51 +0800

    Dice Roller

--- BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
+++ BusinessCard/app/src/main/java/com/example/businesscard/MainActivity.kt
@@ -3,7 +3,6 @@ package com.example.businesscard
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -39,7 +38,6 @@ import com.example.businesscard.ui.theme.BusinessCardTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             BusinessCardTheme {
                 Surface(
